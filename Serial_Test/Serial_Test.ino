79106de97a016654d585bda52b4ba45a9b556e64
samuelgbrown <sambrown@optonline.net>
2016-05-10 10:09:42 -0400

    Processing Functional
    Processing code has been updated to record to .csv properly.  The
    naming functionality has also been implemented.  The GUI is almost
    complete, all that is needed is numerical representations for the
    temperature/humidity, and text to describe the state of the Serial
    connection.  Before full functionality, all warnings must be
    implemented (such as the social time warning).
    
    The Arduino code has been modified to include analog reading of
    temperature and humidity for box 1.  The door-close signal must be
    implemented, as well as the code for all boxes.

--- Serial_Test/Serial_Test.ino
+++ Serial_Test/Serial_Test.ino
@@ -3,6 +3,7 @@ int numBoxes = 3;
 static int bytesPerFloat = 4;
 static int trackedParameters = 3; // Number of parameters that are being sent per box
 static int LF = 10; // Line-feed in ASCII
+static float supply = 5.0;
 
 void setup() {
 
@@ -41,25 +42,11 @@ void sendHandshake() {
 }
 
 void sendNewData() {
-  int variance = 5; // Amount of variance to have in signals
+  int variance = 15; // Amount of variance to have in signals
   // Declare variables to transmit
   byteFloat temp;
   byteFloat humidity;
   byteFloat door;
-
-  // Acquire data from the analog sensors
-  float inputTempVoltage = 71;
-  float inputHumidityVoltage = 60;
-  float inputDoorVoltage = 1;
-
-  // Convert the input from voltage
-  temp.floatVal = tempConvert(inputTempVoltage);
-  humidity.floatVal = humidityConvert(inputHumidityVoltage);
-  door.floatVal = doorConvert(inputDoorVoltage);
-
-  temp.floatVal = (inputTempVoltage);
-  humidity.floatVal = (inputHumidityVoltage);
-  door.floatVal = (inputDoorVoltage);
 
   // Send acquired data to Processing
   // First, send the number of boxes, so Processing knows what to expect
@@ -70,15 +57,57 @@ void sendNewData() {
 
   // Then, for each box, send the necesary information
   for (int i = 0; i < numBoxes; i++) {
-    temp.floatVal += random(0, variance * 10) / 10 - variance/2;
-    humidity.floatVal += random(0, variance * 10) / 10 - variance/2;
-    Serial.write(temp.byteVal, bytesPerFloat);
-    Serial.write(humidity.byteVal, bytesPerFloat);
-    Serial.write(door.byteVal, bytesPerFloat);
+    if (i != 0) {
+      // Acquire data from the analog sensors
+      float inputTempVoltage = 71;
+      float inputHumidityVoltage = 60;
+      float inputDoorVoltage = 1;
+
+      // Convert the input from voltage
+      temp.floatVal = tempConvert(inputTempVoltage);
+      humidity.floatVal = humidityConvert(inputHumidityVoltage);
+      door.floatVal = doorConvert(inputDoorVoltage);
+
+      temp.floatVal = (inputTempVoltage);
+      humidity.floatVal = (inputHumidityVoltage);
+      door.floatVal = (inputDoorVoltage);
+
+      temp.floatVal += random(0, variance * 10) / 10 - variance / 2;
+      humidity.floatVal += random(0, variance * 10) / 10 - variance / 2;
+      Serial.write(temp.byteVal, bytesPerFloat);
+      Serial.write(humidity.byteVal, bytesPerFloat);
+      Serial.write(door.byteVal, bytesPerFloat);
+    } else {
+      temp.floatVal = getTempVal();
+      humidity.floatVal = getHumidityVal(temp.floatVal);
+      door.floatVal = 1;
+
+      Serial.write(temp.byteVal, bytesPerFloat);
+      Serial.write(humidity.byteVal, bytesPerFloat);
+      Serial.write(door.byteVal, bytesPerFloat);
+    }
   }
+
+
 
   // Finally, send a line-feed, to signal the end of the transmission
   Serial.write(LF);
+}
+
+float getTempVal() {
+  float raw = analogRead(0);
+  float rawVolts = raw * (supply / 1023.0); // MORE PRECISE IF USING 3.3V
+  float tempC = (rawVolts - .5) * 100;
+  float tempF = tempC * (9 / 5) + 32;
+  return tempF;
+}
+
+float getHumidityVal(float temp) {
+  float raw = analogRead(1);
+  float rawVolts = raw * (5.0 / 1023.0);
+  float sensorRH = ((rawVolts / supply) - .1515) / .00636;
+  float trueRH = sensorRH / (1.0546 - .00216 * temp);
+  return trueRH;
 }
 
 float tempConvert(float inputVoltage) {
